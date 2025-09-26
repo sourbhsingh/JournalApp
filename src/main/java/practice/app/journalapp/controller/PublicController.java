57775b789c9aa98945a1b6ba56d3a24ad127af09
Sourabh Singh <17sourav.singh@gmail.com>
2025-09-26 16:53:20 +0530

    public controller for user register

--- src/main/java/practice/app/journalapp/controller/PublicController.java
+++ src/main/java/practice/app/journalapp/controller/PublicController.java
@@ -3,10 +3,7 @@ package practice.app.journalapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import practice.app.journalapp.entity.User;
 import practice.app.journalapp.service.UserService;
 
@@ -22,6 +19,7 @@ public class PublicController {
         userService.saveEntry(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+    @GetMapping("/health-check")
     public String healthCheck(){
         return "ok";
     }
