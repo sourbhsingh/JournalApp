8103a5a6e2121b055d46ad147d759b6a2a6fae28
Sourabh Singh <17sourav.singh@gmail.com>
2025-10-07 21:33:34 +0530

    Email Service

---
+++ src/test/java/practice/app/journalapp/service/EmailServiceTests.java
@@ -0,0 +1,17 @@
+package practice.app.journalapp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@SpringBootTest
+public class EmailServiceTests {
+    @Autowired
+    EmailService emailService;
+
+    @Test
+    public void sendMail(){
+        emailService.sendEmail("17sourav.singh@gmail.com","This is Testing Email","Hello Mr Sourabh You are such a great person Thankyou for your hardwork you are puting in for your future self. Your Future self -Sourabh");
+    }
+}
