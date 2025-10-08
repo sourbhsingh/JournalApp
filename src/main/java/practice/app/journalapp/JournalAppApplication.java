9c5c2944bf1b7b5111a21b418e34e469e5bde99a
Sourabh Singh <17sourav.singh@gmail.com>
2025-10-08 20:59:34 +0530

    Enable Scheduling

--- src/main/java/practice/app/journalapp/JournalAppApplication.java
+++ src/main/java/practice/app/journalapp/JournalAppApplication.java
@@ -5,18 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
 public class JournalAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JournalAppApplication.class, args);
     }
     @Bean
-    public PlatformTransactionManager falana(MongoDatabaseFactory dbFactory){
+    public PlatformTransactionManager mongoTM(MongoDatabaseFactory dbFactory){
         return new MongoTransactionManager(dbFactory);
     }
 }
